c572600d72764907e2cac380e873c12d42421a42
FL03 <jo3mccain@icloud.com>
2025-05-30 21:30:48 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- {{ project-name }}/src/error.rs
+++ {{ project-name }}/src/error.rs
@@ -12,19 +12,33 @@ pub type Result<T = ()> = core::result::Result<T, Error>;
 pub enum Error {
     #[error("An error occurred")]
     SomeError,
+    #[cfg(feature = "anyhow")]
+    #[error(transparent)]
+    AnyError(#[from] anyhow::Error),
     #[cfg(feature = "alloc")]
     #[error(transparent)]
     BoxError(#[from] alloc::boxed::Box<dyn core::error::Error + Send + Sync>),
     #[cfg(feature = "json")]
     #[error(transparent)]
     JsonError(#[from] serde_json::Error),
-    #[cfg(feature = "anyhow")]
-    #[error(transparent)]
-    AnyError(#[from] anyhow::Error),
     #[cfg(feature = "std")]
     #[error(transparent)]
     IoError(#[from] std::io::Error),
     #[cfg(feature = "alloc")]
-    #[error(transparent)]
-    UnknownError(#[from] String),
+    #[error("Unknown error: {0}")]
+    UnknownError(String),
+}
+
+#[cfg(feature = "alloc")]
+impl From<String> for Error {
+    fn from(value: String) -> Self {
+        Error::UnknownError(value)
+    }
+}
+
+#[cfg(feature = "alloc")]
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Error::UnknownError(String::from(value))
+    }
 }
--- {{ project-name }}/src/lib.rs
+++ {{ project-name }}/src/lib.rs
@@ -1,17 +1,14 @@
-/*
-    Appellation: {{ project-name }} <library>
-    Contrib: {{ authors }}
-*/
 //! # {{ project-name }}
 //!
 //! {{ description }}
-#![cfg_attr(not(feature = "std"), no_std)]
-#![crate_name = "{{ crate_name }}"]
-#![crate_type = "lib"]
-
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+#[doc(inline)]
+pub use self::error::{Error, Result};
+
 pub mod error;
 
-pub mod prelude {}
+pub mod prelude {
+    pub use crate::error::{Error, Result};
+}
